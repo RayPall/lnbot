014f45fd1e1790b766b61b9a20160db02d850801
RayPall <33890513+RayPall@users.noreply.github.com>
2025-08-05 14:15:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ with tab_post:
             "Čím stylem má být příspěvek napsán?",
             st.session_state.person_list
         )
-        person_mail = st.text_input("E‑mailová adresa")
+        person_mail = st.text_input("E‑mailová adresa (pro odeslání draftu příspěvku)")
         submitted_post = st.form_submit_button("Odeslat")
 
     if submitted_post:
